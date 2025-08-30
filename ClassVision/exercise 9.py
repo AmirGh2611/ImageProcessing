4276935d6199a068e2b2f4f41d1d40633085e629
Amirhossein <amirabbasghorbani354@gmail.com>
2025-08-31 01:47:56 +0330

    fixing the bugs

--- ClassVision/exercise 9.py
+++ ClassVision/exercise 9.py
@@ -1,17 +1,20 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('Data/numbers.jpg', 0)
-blur = cv2.GaussianBlur(img, (9, 9), 0)
-result = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-kernel = np.ones((2, 2), np.uint8)
-closing = cv2.morphologyEx(result, cv2.MORPH_CLOSE, kernel)
-num_labels, labels = cv2.connectedComponents(closing)
-label_hue = np.uint8(179 * labels / np.max(labels))
-blank = np.ones_like(img) * 255
-labeled_img = cv2.merge([label_hue, blank, blank])
-labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-labeled_img[label_hue == 0] = 0
-cv2.imshow('image', labeled_img)
+img_gray = cv2.imread("Data/numbers.jpg", 0)
+blur = cv2.GaussianBlur(img_gray, (9, 9), 0)
+bin_img = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+
+kernel = np.ones((3, 3), np.uint8)
+mask = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel, iterations=1)
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,  kernel, iterations=1)
+
+num, labels, stats, _ = cv2.connectedComponentsWithStats(mask, connectivity=8)
+areas = stats[1:, cv2.CC_STAT_AREA]
+min_area = img_gray.shape[0] * img_gray.shape[1] * 0.001
+count = int((areas >= min_area).sum())
+print(count)
+
+cv2.imshow("mask", mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
