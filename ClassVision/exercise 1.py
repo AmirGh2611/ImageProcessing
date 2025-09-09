a080c0cef01f4c3df85e83cdbe2cf919d7d4e8b2
Amirhossein <amirabbasghorbani354@gmail.com>
2025-09-09 22:21:22 +0330

    deleting class vision

--- ClassVision/exercise 1.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-img = cv2.imread('Data/AdamDriver.png')
-crop = img[180:500, 400:650]
-plt.imshow(img[..., ::-1])
-plt.imshow(crop[..., ::-1])
-plt.show()
-cv2.imwrite('Data/AdamDriver_face.jpg', crop)
